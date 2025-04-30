3f93e6cd26c82960ac0e8074519dd4d9cca95179
Pingu Carsti <ehbrecht@dkrz.de>
2025-04-30 17:58:22 +0200

    added more for cmip6 schema

--- src/piddiplatsch/plugin.py
+++ src/piddiplatsch/plugin.py
@@ -17,17 +17,40 @@ class CMIP6Processor:
     @hookimpl
     def process(self, key, value, handle_client):
         logging.info(f"CMIP6 plugin processing key: {key}")
+
+        item = value["data"]["payload"]["item"]
+
         try:
-            pid = value["data"]["payload"]["item"]["id"]
+            pid = item["id"]
         except KeyError:
             pid = str(uuid.uuid5(uuid.NAMESPACE_DNS, key))
 
         try:
-            url = value["data"]["payload"]["item"]["links"][0]["href"]
+            url = item["links"][0]["href"]
         except (KeyError, IndexError):
             raise ValueError("Missing URL in message")
 
-        record = {"URL": url, "CHECKSUM": None}
+        try:
+            version = item["properties"]["version"]
+        except (KeyError, IndexError):
+            raise ValueError("Missing VERSION in message")
+
+        try:
+            hosting_node = item["assets"]["reference_file"]["alternate:name"]
+        except (KeyError, IndexError):
+            raise ValueError("Missing HOSTING_NODE in message")
+
+        record = {
+            "URL": url,
+            "CHECKSUM": None,
+            "AGGREGATION_LEVEL": "Dataset",
+            "DATASET_ID": pid,
+            "DATASET_VERSION": version,
+            "HOSTING_NODE": hosting_node,
+            "REPLICA_NODE": "",
+            "UNPUBLISHED_REPLICAS": "",
+            "UNPUBLISHED_HOSTS": "",
+        }
         handle_client.add_item(pid, record)
 
 
