c23d666b038820b2502cd58064fad369de070d85
Pingu Carsti <ehbrecht@dkrz.de>
2025-07-28 15:53:20 +0200

    update cmip6 model

--- src/piddiplatsch/models/cmip6_asset_model.py
+++ src/piddiplatsch/models/cmip6_asset_model.py
@@ -1,6 +1,6 @@
-from typing import Optional, Literal
+from typing import Optional, Literal, List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class CMIP6AssetModel(BaseModel):
@@ -10,7 +10,8 @@ class CMIP6AssetModel(BaseModel):
     FILE_NAME: str
     IS_PART_OF: str
     CHECKSUM: Optional[str] = None
+    CHECKSUM_METHOD: Optional[str] = None
     FILE_SIZE: Optional[int] = None
     FILE_VERSION: Optional[str] = None
-    CHECKSUM_METHOD: Optional[str] = None
-    URL_ORIGINAL_DATA: str
+    DOWNLOAD_URL: str
+    DOWNLOAD_URL_REPLICA: List[str] = Field(default_factory=list)
