1858439a4c793828892f5a77e55f9591f2f985b4
Pingu Carsti <ehbrecht@dkrz.de>
2026-01-19 17:03:55 +0100

    removed obsolete plugins package

--- src/piddiplatsch/plugins/cmip6_processor.py
+++
@@ -1,6 +0,0 @@
-"""Deprecated: moved to piddiplatsch.processing.cmip6_processor.
-
-This module remains as a shim for backward compatibility.
-"""
-
-from piddiplatsch.processing.cmip6_processor import CMIP6Processor  # noqa: F401
